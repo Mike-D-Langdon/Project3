424877160d0800f4ede74b1601018d3798322064
margaret-hu <margaret_hu@outlook.com>
2021-04-20 23:06:49 -0700

    Created logout button

--- app/src/main/java/com/example/shoutout/MainActivity.java
+++ app/src/main/java/com/example/shoutout/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.shoutout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +17,7 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String TAG = "MainActivity";
     private Random rand;
 
     @Override
@@ -22,11 +27,27 @@ public class MainActivity extends AppCompatActivity {
         rand = new Random();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            Toast.makeText(this, "Logout button clicked", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
     public void debug_addPost(View view) {
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.layout_timeline_linear, PostFragment.newInstance("https://i.imgur.com/GZQKZ9N.png", "Hello!", "mycoolusername", "whaddup youtube gang its ya boi", "https://i.imgur.com/5oc6cPv.jpg", rand.nextInt(10), rand.nextInt(100), rand.nextInt(100)), UUID.randomUUID().toString())
+                .add(R.id.layout_timeline_linear, PostFragment.newInstance("https://i.imgur.com/GZQKZ9N.png", "Hello!",
+                        "mycoolusername", "whaddup youtube gang its ya boi", "https://i.imgur.com/5oc6cPv.jpg",
+                        rand.nextInt(10), rand.nextInt(100), rand.nextInt(100)), UUID.randomUUID().toString())
                 .addToBackStack(null)
                 .commit();
     }
-
 }
