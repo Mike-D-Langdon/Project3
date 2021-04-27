f8593fc1df4129bf4ba95305226d7357d8293b8d
Mike Langdon <mlangdon2@horizon.csueastbay.edu>
2021-04-27 13:05:03 -0700

    Super basic profile page just to get started with.  Still need to link it to other activities.

---
+++ app/src/main/java/com/example/shoutout/activity/ProfilePageActivity.java
@@ -0,0 +1,16 @@
+package com.example.shoutout.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shoutout.R;
+
+public class ProfilePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+    }
+}
