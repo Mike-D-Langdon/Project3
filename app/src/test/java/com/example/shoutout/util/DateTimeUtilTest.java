2137fc2d2d82655f2b008b86c10ba94192712997
Corneilious Eanes <whizvox0@gmail.com>
2021-04-29 00:32:32 -0700

    Fix DateTimeUtil tests - DateTimeUtil test cases actually pass now

--- app/src/test/java/com/example/shoutout/util/DateTimeUtilTest.java
+++ app/src/test/java/com/example/shoutout/util/DateTimeUtilTest.java
@@ -10,21 +10,21 @@ public class DateTimeUtilTest {
 
     @Test
     public void testParseSimpleDate() {
-        assertEquals(new Date(2000, 1, 1), DateTimeUtil.parseSimpleDate("1/1/2000"));
-        assertEquals(new Date(1900, 6, 7), DateTimeUtil.parseSimpleDate("6/7/1900"));
-        assertEquals(new Date(2038, 2, 20), DateTimeUtil.parseSimpleDate("2/20/2038"));
+        assertEquals(new Date(946713600000L), DateTimeUtil.parseSimpleDate("1/1/2000"));
+        assertEquals(new Date(-2195395200000L), DateTimeUtil.parseSimpleDate("6/7/1900"));
+        assertEquals(new Date(2150265600000L), DateTimeUtil.parseSimpleDate("2/20/2038"));
     }
 
     public void testFormatSimpleDate() {
-        assertEquals("1/1/2000", DateTimeUtil.formatSimpleDate(new Date(2000, 1, 1)));
-        assertEquals("6/7/1900", DateTimeUtil.formatSimpleDate(new Date(1900, 6, 7)));
-        assertEquals("2/20/2038", DateTimeUtil.formatSimpleDate(new Date(2038, 2, 20)));
+        assertEquals("1/1/2000", DateTimeUtil.formatSimpleDate(new Date(946713600000L)));
+        assertEquals("6/7/1900", DateTimeUtil.formatSimpleDate(new Date(-2195395200000L)));
+        assertEquals("2/20/2038", DateTimeUtil.formatSimpleDate(new Date(2150265600000L)));
     }
 
     public void testFormatLongerDate() {
-        assertEquals("Jan 1, 2000", DateTimeUtil.formatSimpleDate(new Date(2000, 1, 1)));
-        assertEquals("Jun 7, 1900", DateTimeUtil.formatSimpleDate(new Date(1900, 6, 7)));
-        assertEquals("Feb 20, 2038", DateTimeUtil.formatSimpleDate(new Date(2038, 2, 20)));
+        assertEquals("Jan 1, 2000", DateTimeUtil.formatLongerDate(new Date(946713600000L)));
+        assertEquals("Jun 7, 1900", DateTimeUtil.formatLongerDate(new Date(-2195395200000L)));
+        assertEquals("Feb 20, 2038", DateTimeUtil.formatLongerDate(new Date(2150265600000L)));
     }
 
 }
